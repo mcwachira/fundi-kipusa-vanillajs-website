387c6553a4f47031c594aa06d052ee38b98813a2
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-05-12 21:41:19 +0300

    made the site responsive

--- js/app.js
+++ js/app.js
@@ -31,7 +31,14 @@ dropDown.addEventListener('click', () => {
 })
 
 
-
+var all = document.getElementsByTagName("*"), i = 0, rect, docWidth = document.documentElement.offsetWidth;
+for (; i < all.length; i++) {
+  rect = all[i].getBoundingClientRect();
+  if (rect.right > docWidth || rect.left < 0) {
+    console.log(all[i]);
+    all[i].style.borderTop = '1px solid red';
+  }
+}
 
 
 
